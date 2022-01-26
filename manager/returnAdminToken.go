9c86b1f5ccbd1dc864e51fe61ec0b0bdc2e7cbe3
barry <barrysunderland@outlook.com>
2022-01-26 21:19:50 +0100

    add instructions on how to replace admin apiKey

--- manager/returnAdminToken.go
+++ manager/returnAdminToken.go
@@ -6,6 +6,12 @@ import (
 	"fmt"
 )
 
+// prints a new admin API Key to the command line
+// useful if admin token needs to be replaced.
+// generate the new token and replace the existing ADMIN_KEY in the .env file
+// delete the existing admin key (with field  permission: admin)
+// restart the service to load the new key from the .env file
+
 func generateAdminAPIToken() string {
 	length := 16
 	b := make([]byte, length)
