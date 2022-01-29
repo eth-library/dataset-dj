8165fd1f1e21d3fd99a8959ec5e55ada5f45324a
barry <barrysunderland@outlook.com>
2022-01-29 20:55:59 +0100

    add testdata for integration testing

---
+++ testsIntegration/mongoInit/importTestData.sh
@@ -0,0 +1,11 @@
+#! bin/bash
+# drop the test db in case this script is run on an existing database
+mongo 
+<<-EOJS
+use test
+db.dropDatabase()
+EOJS
+
+echo "SEEDING TEST DATA"
+mongoimport -d test -c temporaryLinks < /testData/temporaryLinks.json
+mongoimport -d test -c apiKeys < /testData/apiKeys.json
