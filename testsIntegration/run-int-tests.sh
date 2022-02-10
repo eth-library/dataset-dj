fc009eb7736881592a67f8c30ab7c9d5d81f6031
barry <barrysunderland@outlook.com>
2022-02-10 19:07:26 +0100

    use local docker-compose for integration tests

--- testsIntegration/run-int-tests.sh
+++ testsIntegration/run-int-tests.sh
@@ -14,12 +14,13 @@ echo "--- Finished Unit Tests ---"
 
 # run integration tests
 echo "--- Start Integration Tests ---"
-docker-compose -f ../docker-compose.deploy.yml down
-docker-compose -f ../docker-compose.deploy.yml up --build -d
+docker-compose -f ../docker-compose.yml down
+docker-compose -f ../docker-compose.yml up -d --build
 source $(pwd)/../$ENV_FILE && export $(cut -d= -f1 $(pwd)/../$ENV_FILE)
 # wait for services to start
-echo "waiting for backend services"
-sleep 15
+echo "waiting 20s for backend services"
+sleep 20
 go test -v
 echo "--- Finished Integration Tests ---"
-docker-compose -f ../docker-compose.deploy.yml down
+docker-compose -f ../docker-compose.yml stop
+echo "containers are stopped but not removed. To remove them run \n    docker-compose -f ../docker-compose.yml down \nor to inspect logs for a service e.g.: \n    docker logs dataset-dj_taskhandler_1"
